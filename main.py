31b46a5a7d68c6edf38d0ced7bc1dfdce4594215
Neki4ar <Neki4ar@gmail.com>
2023-04-12 22:12:22 +0300

    Рализованы базовые команды для телеграм бота. https://www.youtube.com/watch?v=MEj4J0y4GwU&list=PLNi5HdK6QEmX1OpHj0wvf8Z28NYoV5sBJ&index=4

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from create_bot import dp
+from handlers import client, admin, other
 
-import os
-
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+async def on_startup(_):
+    print('starting bot')
 
 
-@dp.message_handler()
-async def echo_send():
-    pass
+client.register_handlers_client(dp)
+# other.register_handlers_other(dp)
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
+#
+# @dp.message_handler()
+# async def echo_send(message: types.Message):
+#     await message.answer(message.text)
+#     # await message.reply(message.text) # ответ на сообщение
+#     # await bot.send_message(message.from_user.id, message.text) #сообщение в личку
+
+
